512e1e0311ec1d1bce6c0b0882440550bcb95f7c
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-02-04 12:56:03 +0530

    Added new code

--- frontend.sh
+++ frontend.sh
@@ -4,82 +4,46 @@ source ./common.sh
 
 app_name=frontend
 app_dir=/usr/share/nginx/html
+
 check_root
 
-dnf module disable nginx -y &>>$LOGS_FILE
-dnf module enable nginx:1.24 -y &>>$LOGS_FILE
-dnf install nginx -y &>>$LOGS_FILE
-VALIDATE $? "Installing Nginx"
+dnf list installed nginx &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+    dnf module disable nginx -y &>>$LOGS_FILE
+    VALIDATE $? "Disabling NGINX Module"
 
-systemctl enable nginx  &>>$LOGS_FILE
-systemctl start nginx 
-VALIDATE $? "Enabled and started nginx"
+    dnf module enable nginx:1.24 -y &>>$LOGS_FILE
+    VALIDATE $? "Enabling NGINX 1.24 Module"
 
-rm -rf /usr/share/nginx/html/* 
-VALIDATE $? "Remove default content"
+    dnf install nginx -y &>>$LOGS_FILE
+    VALIDATE $? "Installing NGINX..."
+else
+    echo -e "NginX already installed.... $Y SKYPPING $N"
+fi
+systemctl enable nginx &>>$LOGS_FILE
+VALIDATE $? "Enabling NGINX"
+
+systemctl start nginx &>>$LOGS_FILE
+VALIDATE $? "Starting NGINX"
+
+rm -rf /usr/share/nginx/html/* &>>$LOGS_FILE
+VALIDATE $? "Remove default content."
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOGS_FILE
-cd /usr/share/nginx/html 
+VALIDATE $? "Downloading frontend code"
+
+cd $app_dir &>>$LOGS_FILE
+VALIDATE $? "Going to html folder"
+
 unzip /tmp/frontend.zip &>>$LOGS_FILE
-VALIDATE $? "Downloaded and unzipped frontend"
+VALIDATE $? "Unziping frontend code"
 
 rm -rf /etc/nginx/nginx.conf
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
-VALIDATE $? "Copied our nginx conf file"
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOGS_FILE
+VALIDATE $? "Copying NGINX Config File"
 
-systemctl restart nginx
-VALIDATE $? "Restarted Nginx"
+systemctl restart nginx  &>>$LOGS_FILE
+VALIDATE $? "Restarting NGINX Service"
 
 print_total_time
-
-
-# #!/bin/bash
-
-# source ./common.sh
-
-# app_name=frontend
-# app_dir=/usr/share/nginx/html
-
-# check_root
-
-# dnf list installed nginx &>>$LOGS_FILE
-# if [ $? -ne 0 ]; then
-#     dnf module disable nginx -y &>>$LOGS_FILE
-#     VALIDATE $? "Disabling NGINX Module"
-
-#     dnf module enable nginx:1.24 -y &>>$LOGS_FILE
-#     VALIDATE $? "Enabling NGINX 1.24 Module"
-
-#     dnf install nginx -y &>>$LOGS_FILE
-#     VALIDATE $? "Installing NGINX..."
-# else
-#     echo -e "NginX already installed.... $Y SKYPPING $N"
-# fi
-# systemctl enable nginx &>>$LOGS_FILE
-# VALIDATE $? "Enabling NGINX"
-
-# systemctl start nginx &>>$LOGS_FILE
-# VALIDATE $? "Starting NGINX"
-
-# rm -rf /usr/share/nginx/html/* &>>$LOGS_FILE
-# VALIDATE $? "Remove default content."
-
-# curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOGS_FILE
-# VALIDATE $? "Downloading frontend code"
-
-# cd $app_dir &>>$LOGS_FILE
-# VALIDATE $? "Going to html folder"
-
-# unzip /tmp/frontend.zip &>>$LOGS_FILE
-# VALIDATE $? "Unziping frontend code"
-
-# rm -rf /etc/nginx/nginx.conf
-
-# cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOGS_FILE
-# VALIDATE $? "Copying NGINX Config File"
-
-# systemctl restart nginx  &>>$LOGS_FILE
-# VALIDATE $? "Restarting NGINX Service"
-
-# print_total_time
