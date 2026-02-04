84f56532c27c9c66fd0afb9a2cb52394b16b8def
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-02-04 11:38:52 +0530

    Added new code

--- user.sh
+++ user.sh
@@ -7,9 +7,5 @@ app_name=user
 check_root
 nodejs_setup
 app_setup
-
-cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
-VALIDATE $? "Copying service file"
-
 systemd_setup
 print_total_time
