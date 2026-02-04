f021fca7433780b3da8ae230fe9d16ef79ab8d72
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-02-04 11:53:09 +0530

    Added new code

--- mysql.sh
+++ mysql.sh
@@ -1,31 +1,9 @@
 #!/bin/bash
 
-USER_ID=$(id -u)
-LOGS_FOLDER="/var/log/shell-roboshop"
-LOGS_FILE="$LOGS_FOLDER/$0.log"
-mysql_root_password="RoboShop@1"
-DOMAIN_NAME="mysql.dawsmani.site"
+source ./common.sh
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-if [ $USER_ID -gt 0 ]; then
-    echo -e " $R Please run this script with root user :) $N" | tee -a $LOGS_FILE
-    exit 3;
-fi
-
-mkdir -p $LOGS_FOLDER
-
-# tee command is used to write the output to log file as well as to the console
-VALIDATE(){  
-    if [ $1 -eq 0 ]; then
-        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
-    else 
-        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
-    fi
-}
+app_name=mysql
+check_root
 
 dnf list installed mysql-server &>>$LOGS_FILE
 if [ $? -eq 0 ]; then
@@ -50,3 +28,5 @@ then
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
 fi
+
+print_total_time
