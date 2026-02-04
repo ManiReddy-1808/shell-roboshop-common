85a4cd4e032f3f7981e1ff6491f0e353a04e8bd0
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-02-04 12:18:18 +0530

    Added new code

--- common.sh
+++ common.sh
@@ -76,6 +76,17 @@ app_setup(){
     VALIDATE $? "Extracting $app_name App Code"
 }
 
+python_setup(){
+    dnf install python3 gcc python3-devel -y &>>$LOGS_FILE
+    VALIDATE $? "Installing Python3 and Build Tools"
+
+    cd /app 
+    VALIDATE $? "Changing Directory to /app"
+
+    pip3 install -r requirements.txt &>>$LOGS_FILE
+    VALIDATE $? "Installing Python Dependencies"
+}
+
 java_setup(){
     dnf list installed maven &>>$LOGS_FILE
     if [ $? -eq 0 ]; then
